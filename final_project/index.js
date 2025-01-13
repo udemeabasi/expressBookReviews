ff088835f011b338ac981bbade32656cbf4b76ea
udemeabasi <udemeabasi6@gmail.com>
2025-01-12 21:04:34 -0500

    Yes

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,31 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+// Login endpoint
+app.post("/login", (req, res) => { 
+const username = req.body.username; 
+const password = req.body.password; 
+// Check if username or password is missing 
+if (!username || !password) { 
+return res.status(404).json({ message: "Error logging in" }); 
+} 
+
+// Authenticate user 
+if (authenticatedUser(username, password)) { 
+// Generate JWT access token 
+let accessToken = jwt.sign({ 
+data: password 
+}, 'access', { expiresIn: 60 * 60 }); 
+// Store access token and username in session 
+req.session.authorization = { 
+accessToken, username 
+} 
+return res.status(200).send("User successfully logged in"); 
+} else { 
+return res.status(208).json({ message: "Invalid Login. Check username and password" }); 
+}
+}); 
+
 const token = req.headers['authorization'];
 
 if (!token) {
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,20 +13,30 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+  return res.status(300).json({message: book});
+  } else {
+    res.status(404).json({message: "book not found"})
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const author = req.params.author.toLowerCase();
+  const booksByAuthor = books.filter(book => book.author.toLowerCase() === author);
+  
+ return res.status(300).json({message: booksByAuthor});
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
